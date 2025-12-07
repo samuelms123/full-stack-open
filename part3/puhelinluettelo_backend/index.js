964de89b8ca730a894283420a389e4f91daf71af
Samuel Sarimo <samuelsarimo@Samuels-MacBook-Air.local>
2025-12-07 11:30:01 +0200

    Add env port

--- part3/puhelinluettelo_backend/index.js
+++ part3/puhelinluettelo_backend/index.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // enable CORS
 app.use(cors());
